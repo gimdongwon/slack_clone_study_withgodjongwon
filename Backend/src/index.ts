9e987f94404c46dd72a778234e4157102cef7ba1
kimdongwon <ehddnjs8989@gmail.com>
2020-01-14 12:34:12 +0900

    # BE 7. create graphql types

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,21 +1,22 @@
 import { GraphQLServer } from 'graphql-yoga';
 import connection from './ormConfig';
+import schema from './schema';
 
-const typeDefs = `
-    type Query{
-        sayHello : String!
-    }
-`;
+// const typeDefs = `
+//     type Query{
+//         sayHello : String!
+//     }
+// `;
 
-// 인자값과 리턴되는 값의 타입을 지정
+// // 인자값과 리턴되는 값의 타입을 지정
 
-const resolvers = {
-	Query: {
-		sayHello: () => 'Hi there ㅇㅅㅇa'
-	}
-};
-// 함수의 비즈니스 로직이 실제로 들어가는 부분
+// const resolvers = {
+// 	Query: {
+// 		sayHello: () => 'Hi there ㅇㅅㅇa'
+// 	}
+// };
+// // 함수의 비즈니스 로직이 실제로 들어가는 부분
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ schema });
 
 connection.then(() => server.start(() => console.log('Hello GraphQL server')));
