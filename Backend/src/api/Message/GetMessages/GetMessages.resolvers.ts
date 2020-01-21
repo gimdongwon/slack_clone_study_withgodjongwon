826a7ba89f9a95e766d46b194210e80307af3120
kimdongwon <ehddnjs8989@gmail.com>
2020-01-21 12:39:19 +0900

    #BE.9 set Graphql to typescript Fnc types

---
+++ Backend/src/api/Message/GetMessages/GetMessages.resolvers.ts
@@ -0,0 +1,29 @@
+import { GetMessagesQueryArgs, GetMessagesResponse } from './../../../types/graphql.d';
+import { Resolvers } from './../../../types/resolvers.d';
+import Message from '../../../../src/entities/Message';
+const resolvers: Resolvers = {
+	Query: {
+		GetMessages: async (_, args: GetMessagesQueryArgs): Promise<GetMessagesResponse> => {
+			// logic start
+			try {
+				const { innerChannelId } = args;
+				// == const innerChannelId = args.innerChannelId
+
+				const messages = await Message.find({ innerChannelId });
+				return {
+					ok: true,
+					error: null,
+					messages
+				};
+			} catch (error) {
+				return {
+					ok: false,
+					error: error.message,
+					messages: null
+				};
+			}
+		}
+	}
+};
+
+export default resolvers;
---
+++ Backend/src/types/graphql.d.ts
@@ -0,0 +1,34 @@
+export const typeDefs = ["type Channel {\n  id: Int!\n  channelName: String!\n  messages: [Message]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype Query {\n  GetMessages(innerChannelId: Int!): GetMessagesResponse!\n}\n\ntype Message {\n  id: Int!\n  nickname: String!\n  contents: String!\n  innerChannel: Channel!\n  innerChannelId: Int!\n  createdAt: String!\n  updatedAt: String\n}\n"];
+/* tslint:disable */
+
+export interface Query {
+  GetMessages: GetMessagesResponse;
+}
+
+export interface GetMessagesQueryArgs {
+  innerChannelId: number;
+}
+
+export interface GetMessagesResponse {
+  ok: boolean;
+  error: string | null;
+  messages: Array<Message> | null;
+}
+
+export interface Message {
+  id: number;
+  nickname: string;
+  contents: string;
+  innerChannel: Channel;
+  innerChannelId: number;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Channel {
+  id: number;
+  channelName: string;
+  messages: Array<Message> | null;
+  createdAt: string;
+  updatedAt: string | null;
+}
